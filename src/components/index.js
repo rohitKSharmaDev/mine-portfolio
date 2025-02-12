4777208bf42e68a50871e7fe365b69b95429a826
Rohit Sharma <rohitksharmadev@gmail.com>
2025-02-12 22:26:43 +0530

    Finish Hero Section, create and finish About section of my portfolio.

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
 import Hero from './Hero';
 import Navbar from './Navbar';
-import About from './About';
 import Tech from './Tech';
 import Experience from './Experience';
 import Works from './Works';
@@ -11,7 +10,6 @@ import Contact from './Contact';
 export {
   Hero,
   Navbar,
-  About,
   Tech,
   Experience,
   Works,
@@ -22,3 +20,5 @@ export {
   ComputersCanvas, 
   StarsCanvas
 }
+
+export { default as About } from './About';
